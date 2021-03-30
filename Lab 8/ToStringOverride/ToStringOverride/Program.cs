87cdfce8003428c1477d260eff2ff4699f659940
vikasviks <67867372+vikasviks@users.noreply.github.com>
2021-03-31 00:03:42 +0530

    overriding ToString() method

---
+++ Lab 8/ToStringOverride/ToStringOverride/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ToStringOverride
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product obj = new Product {
+                Id = 1,
+                Title = "Pen",
+                Price = 50
+            };
+            Console.WriteLine(obj.ToString());
+        }
+    }
+}
