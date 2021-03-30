8029ae243d02eac55c5b7fd152af44eb7583f16f
vikasviks <67867372+vikasviks@users.noreply.github.com>
2021-03-31 00:10:47 +0530

    lambda expression and LINQ query  

---
+++ Lab 16/LinQDemo/LinQDemo/Products.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinQDemo
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public string Title { get; set; }
+        public string Owner { get; set; }
+        public Products(int id, int p, string t, string o)
+        {
+            Id = id;
+            Price = p;
+            Title = t;
+            Owner = o;
+        }
+        public override string ToString()
+        {
+            return $"ID = {Id} Price = {Price} Title = {Title} Owner = {Owner}"; 
+        }
+    }
+}
