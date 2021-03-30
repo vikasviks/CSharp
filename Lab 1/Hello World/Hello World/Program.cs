fed6a940eebf0747a41231e8a21a0025e902cf2b
vikasviks <67867372+vikasviks@users.noreply.github.com>
2021-03-30 23:55:55 +0530

    Add files via upload
    Hello World in C#

---
+++ Lab 1/Hello World/Hello World/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello There!");
+            Console.WriteLine("Enter your name");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"Hello {name} on {date}");
+            Console.ReadKey(true);
+        }
+    }
+}
