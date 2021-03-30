4c0b5be4487446fe29daf2301fb1708f9b8d635d
vikasviks <67867372+vikasviks@users.noreply.github.com>
2021-03-31 00:09:26 +0530

    Creating a Generics class

---
+++ Lab 15/GenericsDemo/GenericsDemo/Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public override string ToString()
+        {
+            string s = "Id: " + this.Id + " " + "Name: " + this.Name + " " + "Price: " + this.Price;
+            return s;
+        }
+
+    }
+}
---
+++ Lab 15/GenericsDemo/GenericsDemo/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class GenericClass<T>
+    {
+
+        public void display(T item)
+        {
+            Console.WriteLine(item);
+        }
+
+
+    }
+}
