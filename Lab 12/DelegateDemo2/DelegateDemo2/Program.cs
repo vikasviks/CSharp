8f3955f8f6fac9dd94889df240d03f8bdedb273f
vikasviks <67867372+vikasviks@users.noreply.github.com>
2021-03-31 00:07:13 +0530

    Calculator with delegate print output

---
+++ Lab 12/DelegateDemo2/DelegateDemo2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DelegateDemo2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Calculator newCal = new Calculator
+            {
+                Num1 = 100,
+                Num2 = 50
+            };
+            Calculator.CalDelegate c =  newCal.add;
+            newCal.printRes(c);
+
+            c = newCal.sub;
+            newCal.printRes(c);
+
+            c = newCal.mul;
+            newCal.printRes(c);
+
+            c = newCal.div;
+            newCal.printRes(c);
+        }
+    }
+}
