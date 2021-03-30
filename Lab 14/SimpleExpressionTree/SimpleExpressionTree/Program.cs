0d2b05a96be6b44ced52b078ef6fd009c031d65c
vikasviks <67867372+vikasviks@users.noreply.github.com>
2021-03-31 00:08:44 +0530

    Simple Expression Tree and using Lambda Expression

---
+++ Lab 14/SimpleExpressionTree/SimpleExpressionTree/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SimpleExpressionTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Linq.Expressions.Expression<Func<int, int>> e = (x) => x * x ;
+            Console.WriteLine(e);
+
+            var a = e.Compile();
+            Console.WriteLine(a(10));
+
+            Func<int, int> sq = (x) => x * x;
+            Console.WriteLine(sq(12));
+        }
+    }
+}
